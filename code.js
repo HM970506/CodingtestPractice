170d3e16236f91af85b3b7edec0e2e13edd58a53
HM970506 <62527898+HM970506@users.noreply.github.com>
2022-08-17 19:24:36 +0900

    재귀2번

--- code.js
+++ code.js
@@ -27,13 +27,18 @@ const real="/dev/stdin"
 
 
 
-function recursion(n, m, layer, string, check){
-if(layer>=m) {console.log(string.slice(0, string.length-1)); return;}
-for(let i=0; i<n; i++){
+function recursion(n, m, layer, array, check){
+if(layer>=m) {console.log(array.sort(
+    (a, b)=>{if(a>b)return 1; else return -1;}
+).join(" ")); return;}
+
+for(let i=layer; i<n; i++){
+  
   if(check[i]){
       check[i]=false;
-      recursion(n, m, layer+1,string+(i+1)+" ", [...check]);
-      check[i]=true;
+      array.push(i+1);
+      recursion(n, m, layer+1, [...array], [...check]);
+      array.pop();
   }
 }
 }
@@ -44,4 +49,4 @@ for(let i=0; i<x[0]; i++){
 check.push(true);
 }
 
-recursion(x[0], x[1], 0, "", [...check]);
+recursion(x[0], x[1], 0, [], [...check]);
