2064050955721c6b58b89dd90850233c97bf6807
HM970506 <62527898+HM970506@users.noreply.github.com>
2022-08-17 20:28:09 +0900

    재귀7번

--- code.js
+++ code.js
@@ -26,28 +26,19 @@ const test="./input.txt"
 const real="/dev/stdin"
 
 
-function recursion(n, m, layer, string, array, check, answer){
-  if(layer>=m) {
-    if(!answer.includes(string.join(" "))) answer.push(string.join(" ")); 
-  return;}
+function recursion(n, m, layer, string, array, answer){
+  if(layer>=m) {answer.push(string.join(" ")); return;}
   for(let i=0; i<n; i++){
-    if(check[i]){
-      check[i]=false;
       string.push(array[i]);
-      recursion(n, m, layer+1, [...string], array, [...check], answer);
+      recursion(n, m, layer+1, [...string], array,  answer);
       string.pop();
-    }
   }
 }
 const x=inputs(real);
-const array=x[1].sort((a,b)=>{if(parseInt(a)>parseInt(b))return 1; else return -1});
-const check=[];
+const array=Array.from(new Set(x[1])).sort((a,b)=>{if(parseInt(a)>parseInt(b))return 1; else return -1});
 const answer=[];
 
-for(let i=0; i<x[0][0]; i++){
-  check.push(true);
-}
 
-recursion(x[0][0], x[0][1], 0, [], array, check, answer);
+recursion(array.length, x[0][1], 0, [], array, answer);
 
-console.log(answer.join("\n"));
+console.log(Array.from(new Set(answer)).join("\n"));
