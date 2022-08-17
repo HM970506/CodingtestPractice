b4faf4c27ddff27670f691baa1c45a768634875d
HM970506 <62527898+HM970506@users.noreply.github.com>
2022-08-17 20:32:27 +0900

    재귀8번

--- code.js
+++ code.js
@@ -26,19 +26,19 @@ const test="./input.txt"
 const real="/dev/stdin"
 
 
-function recursion(n, m, layer, string, array, answer){
+function recursion(n, m, now, layer, string, array, answer){
   if(layer>=m) {answer.push(string.join(" ")); return;}
-  for(let i=0; i<n; i++){
+  for(let i=now; i<n; i++){
       string.push(array[i]);
-      recursion(n, m, layer+1, [...string], array,  answer);
+      recursion(n, m,i, layer+1, [...string], array, answer);
       string.pop();
   }
 }
-const x=inputs(real);
+const x=inputs(test);
 const array=Array.from(new Set(x[1])).sort((a,b)=>{if(parseInt(a)>parseInt(b))return 1; else return -1});
 const answer=[];
 
 
-recursion(array.length, x[0][1], 0, [], array, answer);
+recursion(array.length, x[0][1], 0, 0, [], array, answer);
 
 console.log(Array.from(new Set(answer)).join("\n"));
